2cca7574ff0a3ec635cfbad5cc34ff0de12ec9f2
DESKTOP-325D3AA\Poom <poom_se@cmu.ac.th>
2020-08-21 15:28:33 +0700

    Lab5

---
+++ Lab5_612110071/Assets/_Character Models and Animations/Ch_24Control.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ch_24Control : MonoBehaviour
+{
+    Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _animator.SetTrigger("Dodge");
+        }
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            _animator.SetTrigger("Pray");
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            _animator.SetTrigger("Loser");
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            _animator.SetTrigger("Taunt");
+        }
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            _animator.SetBool("Dancing", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftAlt))
+        {
+            _animator.SetBool("Dancing", false);
+        }
+    }
+}
